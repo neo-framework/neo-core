660c391617b83abe2f6f4ff7d46ad93ba17d7f11
YouniS Bensalah <younis.bensalah@gmail.com>
2016-05-01 23:08:21 +0200

    Add factory interface

--- src/neo/factory/FactoryInterface.php
+++ src/neo/factory/FactoryInterface.php
@@ -1,1 +1,24 @@
- 
+<?php
+
+/*!
+ * Neo Framework (https://neo-framework.github.io)
+ *
+ * Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace neo\factory;
+
+/*!
+ * Factory interface.
+ */
+interface FactoryInterface {
+
+    /*!
+     * Return the requested instance.
+     */
+    public function factor($classname, array $args = null);
+
+}
