ed4e39e8f8670f8e426f0e37895b7a4f3ee9e488
YouniS Bensalah <younis.bensalah@gmail.com>
2016-05-01 23:08:34 +0200

    Add view interface

--- src/neo/view/ViewInterface.php
+++ src/neo/view/ViewInterface.php
@@ -1,1 +1,18 @@
- 
+<?php
+
+/*!
+ * Neo Framework (https://neo-framework.github.io)
+ *
+ * Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace neo\view;
+
+interface ViewInterface {
+
+    public function display();
+
+}
