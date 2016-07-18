89d595e6cdcf11c8b57949d61055ed062613cadd
YouniS Bensalah <younis.bensalah@gmail.com>
2016-07-18 14:48:21 +0200

    Add Router and tests Get rid of old config stuff

--- src/neo/router/Router.php
+++ src/neo/router/Router.php
@@ -1,96 +1,60 @@
 <?php
 
-/*!
- * Neo Framework (https://neo-framework.github.io)
+/**
+ * Neo Framework
  *
- * Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @link https://neo-framework.github.io
+ * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ * @license MIT
  */
 
 namespace neo\router;
 
 use \Klein\Klein;
-use \neo\factory\FactoryInterface;
+use \neo\factory\FactoryInterface as Factory;
 use \neo\exception\CoreException;
 
-/*!
+/**
  * The Router forwards queries to the right controller.
+ * Here, we are using Klein as a backend to handle the routing.
  */
-class Router {
+class Router
+{
 
     private $klein;
 
     private $controller_factory;
 
-    /*!
-     * klein: Instance of Klein router
-     *
-     * routes: Key-value config array containing all routes
-     *
-     * controller_factory: Controller factory used to create controller objects on demand
-     */
-    public function __construct(Klein $klein, array $routes, FactoryInterface $controller_factory)
+    public function __construct(Klein $klein, Factory $controller_factory)
     {
         $this->klein = $klein;
         $this->controller_factory = $controller_factory;
-        $this->map($routes);
     }
 
-    /*!
+    /**
      * Find a matching route for the request and run it.
      */
     public function dispatch()
     {
         $this->klein->onHttpError(function ($code) {
-            die("<h1>Error $code</h1>");
+            throw new CoreException('HTTP Error ' . $code);
         });
         $this->klein->dispatch();
     }
 
-    /*!
-     * Map each route to a callable that will initiate the controller.
+    /**
+     * Map an http method and route to an action and controller.
      */
-    private function map($routes)
+    public function map($method, $route, $action, $controller)
     {
-        $controller_factory = $this->controller_factory;
+        $cf = $this->controller_factory;
 
-        foreach ($routes as $route => $target) {
-            // split into http methods and controller
-            $target = \explode(' ', $target);
-            if (\count($target) !== 2) {
-                throw new CoreException("Could not split HTTP methods and Controller: Invalid route syntax.");
-            }
-
-            // extract http methods
-            $method = \array_values(\array_intersect(\explode('|', $target[0]),
-                ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']));
-            if (\count($method) === 0) {
-                throw new CoreException("Could not extract HTTP methods: Invalid route syntax.");
-            }
-
-            // split controller into class and function
-            $controller = \explode('@', $target[1]);
-            if (\count($controller) !== 2) {
-                throw new CoreException("Could not split Controller into class and function: Invalid route syntax.");
-            }
-
-            // replace angular brackets by square brackets
-            $route = \str_replace('<', '[', $route);
-            $route = \str_replace('>', ']', $route);
-
-            $dispatch = $controller[0];
-            $controller = $controller[1];
-
-            // register route to klein
-            $this->klein->respond($method, $route,
-                function ($request) use ($controller_factory, $dispatch, $controller) {
-                    $c = $controller_factory->factor($controller);
-                    $c->setRequest($request);
-                    return $c->$dispatch();
-                });
-        }
+        $this->klein->respond($method, $route, function ($request) use ($cf, $action, $controller) {
+            $controller .= 'Controller';
+            $action .= '_action';
+            $c = $cf->factor($controller, [$request]);
+            return $c->$action();
+        });
     }
 
 }
---
+++ tests/RouterTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Neo Framework
+ *
+ * @link https://neo-framework.github.io
+ * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ * @license MIT
+ */
+
+namespace neo\tests;
+
+use \neo\router\Router;
+use \Klein\Klein;
+
+class RouterTest extends \PHPUnit_Framework_TestCase {
+
+    protected $router;
+
+    protected function setUp()
+    {
+        // create a controller mock
+        $controller_mock = $this->getMockBuilder('\\neo\\controller\\AbstractController')
+            ->setMethods(['foo_action'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $controller_mock->method('foo_action')
+            ->willReturn("Hellowz");
+        // create a controller factory mock that returns the controller
+        $factory_mock = $this->getMockBuilder('\\neo\\factory\\FactoryInterface')
+            ->getMock();
+        $factory_mock->method('factor')
+            ->willReturn($controller_mock);
+        // create router
+        $this->router = new Router(new Klein(), $factory_mock);
+    }
+
+    public function testSimpleRequest()
+    {
+        // map GET /test to Bar::foo()
+        $this->router->map('GET', '/test', 'foo', 'Bar');
+
+        // lel
+        $_SERVER['REQUEST_URI'] = '/test';
+
+        $this->expectOutputString("Hellowz");
+        $this->router->dispatch();
+    }
+
+}
