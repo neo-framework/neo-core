b1b43a7d4953633d09c4903d5063c6833b0f3876
YouniS Bensalah <younis.bensalah@gmail.com>
2016-07-18 14:49:35 +0200

    Add AbstractController

---
+++ src/neo/controller/AbstractController.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Neo Framework
+ *
+ * @link https://neo-framework.github.io
+ * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ * @license MIT
+ */
+
+namespace neo\controller;
+
+use \Klein\Request;
+
+abstract class AbstractController
+{
+
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+}
