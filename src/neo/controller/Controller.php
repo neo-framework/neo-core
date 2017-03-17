07500d1e47f04459b51d534803f54f70a96e3ee5
YouniS Bensalah <younis.bensalah@gmail.com>
2017-03-17 20:41:33 +0100

    Add Response to Controller

--- src/neo/controller/Controller.php
+++ src/neo/controller/Controller.php
@@ -4,22 +4,25 @@
  * Neo Framework
  *
  * @link https://neo-framework.github.io
- * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ * @copyright Copyright (c) 2016-2017 YouniS Bensalah <younis.bensalah@gmail.com>
  * @license MIT
  */
 
 namespace neo\controller;
 
 use \Klein\Request;
+use \Klein\Response;
 
 abstract class Controller
 {
 
-    private $request;
+    protected $request;
+    protected $response;
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
 }
