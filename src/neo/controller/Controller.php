13b43e1799a413fb6787eebfda0c8b52a6b05e13
YouniS Bensalah <younis.bensalah@gmail.com>
2017-03-17 22:23:24 +0100

    Add view() hack thx @fwhcat lol

--- src/neo/controller/Controller.php
+++ src/neo/controller/Controller.php
@@ -13,6 +13,7 @@ namespace neo\controller;
 use \Klein\Request;
 use \Klein\Response;
 use \endobox\Factory as Endobox;
+use \endobox\Box;
 
 abstract class Controller
 {
@@ -29,5 +30,26 @@ abstract class Controller
         $this->response = $response;
         $this->view = $endobox;
     }
+
+    /**
+     * Usually, this is what I want to do:
+     *
+     * $test = $endobox('mytemplate');
+     *
+     * Now, $endobox is no longer a simple variable, but a property:
+     *
+     * $test = $this->view('mytemplate');
+     *
+     * Except PHP will now think that we're trying to call a method named view() which does not exist.
+     * To fix this we'd have to write explicit parenthesis like this:
+     *
+     * $test = ($this->view)('mytemplate');
+     *
+     * But that's pretty ugly and that's why this method exists.
+     */
+     public function view(string $template) : Box
+     {
+         return ($this->view)($template);
+     }
 
 }
