3e90514c30130bc2e56e7bcea95f109c0303220c
YouniS Bensalah <younis.bensalah@gmail.com>
2017-03-17 20:41:09 +0100

    Update factory code

--- src/neo/factory/ControllerFactory.php
+++ src/neo/factory/ControllerFactory.php
@@ -4,30 +4,39 @@
  * Neo Framework
  *
  * @link https://neo-framework.github.io
- * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ * @copyright Copyright (c) 2016-2017 YouniS Bensalah <younis.bensalah@gmail.com>
  * @license MIT
  */
 
 namespace neo\factory;
 
+use \Klein\Request;
+use \Klein\Response;
+
 /**
- * Controller factory.
+ * This is the class that knows how controllers are constructed.
  */
-class ControllerFactory implements Factory
+class ControllerFactory extends ContainerAwareFactory
 {
 
-    public function make(string $classname, array $args = null)
+    public function __invoke(string $classname, array $args = [])
     {
+        $classname = $this->container['config']['global']['app_ns'] . '\\controllers\\' . $classname;
+
         if (!\class_exists($classname)) {
             throw new \RuntimeException(\sprintf(
-                'Could not instantiate controller: Class "%s" does not exist.', $classname));
+                    'Could not instantiate controller: Class "%s" does not exist.', $classname));
         }
 
-        if (!isset($args[0])) {
-            throw new \RuntimeException('Could not instantiate controller: Missing request argument.');
+        if (!(isset($args[0]) && $args[0] instanceof Request)) {
+            throw new \RuntimeException('Could not instantiate controller: Expected args[0] to be of type Request.');
         }
 
-        return new $classname($args[0]);
+        if (!(isset($args[1]) && $args[1] instanceof Response)) {
+            throw new \RuntimeException('Could not instantiate controller: Expected args[1] to be of type Response.');
+        }
+
+        return new $classname($args[0], $args[1]);
     }
 
 }
--- src/neo/factory/Factory.php
+++ src/neo/factory/Factory.php
@@ -4,21 +4,15 @@
  * Neo Framework
  *
  * @link https://neo-framework.github.io
- * @copyright Copyright (c) 2016 YouniS Bensalah <younis.bensalah@gmail.com>
+ * @copyright Copyright (c) 2016-2017 YouniS Bensalah <younis.bensalah@gmail.com>
  * @license MIT
  */
 
 namespace neo\factory;
 
-/**
- * Factory interface.
- */
 interface Factory
 {
 
-    /**
-     * Return the requested instance.
-     */
-    public function make(string $classname, array $args = null);
+    public function __invoke(string $classname, array $args = []);
 
 }
