e01565f2a81618a8229844eb555d638125f79bcb
YouniS Bensalah <younis.bensalah@gmail.com>
2019-01-27 18:39:50 +0100

    Copyright s/2018/2019

--- src/neo/core/controller/ControllerFactory.php
+++ src/neo/core/controller/ControllerFactory.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of Neo Framework.
  *
- * (c) 2016-2018 YouniS Bensalah <younis.bensalah@gmail.com>
+ * (c) 2016-2019 YouniS Bensalah <younis.bensalah@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
